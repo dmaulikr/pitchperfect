a3b60c9ac6f84ded71705e501048eda120808823
Josh Schindler <js2387@att.com>
2017-07-31 12:38:41 -0400

    set contentMode to .scaleAspectFit so buttons aprreared better on small screens

--- Pitch Perfect/playSoundsViewController.swift
+++ Pitch Perfect/playSoundsViewController.swift
@@ -56,6 +56,13 @@ class PlaySoundsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupAudio()
+        snailButton.imageView?.contentMode = .scaleAspectFit
+        chipmunkButton.imageView?.contentMode = .scaleAspectFit
+        rabbitButton.imageView?.contentMode = .scaleAspectFit
+        vaderButton.imageView?.contentMode = .scaleAspectFit
+        echoButton.imageView?.contentMode = .scaleAspectFit
+        reverbButton.imageView?.contentMode = .scaleAspectFit
+        stopButton.imageView?.contentMode = .scaleAspectFit
         
         // Do any additional setup after loading the view.
     }
